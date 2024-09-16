23d48212a391875867e8167c385f32061ef0dfa8
daid7 <daid7@mcmaster.ca>
2024-09-16 13:49:04 -0400

    Add files via upload

---
+++ 2XC3-labs/week1/lab2hw.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(){
+
+    printf("Danny Dai\n");
+    sleep(3);
+    printf("400505160\n");
+    sleep(3);
+    printf("SE2XC3\n");
+    sleep(3);
+    puts("'cd ..' to return to the upper directory");
+    sleep(3);
+    puts("'mkdir' kind of just makes a folder, that you can do in finder as well");
+    sleep(3);
+    puts("Vim is much more connected to the terminal, which allows clear understanding and lower level manipulation of code's location");
+    sleep(3);
+    puts("VS Code lets you use your mouse, instead of just moving by arrow keys, which is way better. I cannot run linux terminal though, so I still need to connect ssh on VS Code.");
+    sleep(3);
+    
+    return EXIT_SUCCESS;
+
+}
